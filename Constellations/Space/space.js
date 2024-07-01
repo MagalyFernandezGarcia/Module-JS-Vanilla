67350e19285edaca80a01ca1fb27cecfaa3c4799
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-07-01 16:41:01 +0200

    fixing bug of array[0].image

--- Constellations/Space/space.js
+++ Constellations/Space/space.js
@@ -2,29 +2,7 @@ const IMG_CONSTELLATION = document.getElementById("imgConstellation");
 const PREVIOUS = document.getElementById("previous");
 const NEXT = document.getElementById("next");
 const jsonAPi = "http://localhost:3000/";
-
-// let arrayOfImages = [
-// 	"../images/aries-constellation-01.jpg",
-// 	"../images/taurus.jpg",
-// ];
-
-// let currentIndex = 0;
-
-// NEXT.addEventListener("click", () => {
-// 	currentIndex++;
-// 	if (currentIndex === arrayOfImages.length) {
-// 		currentIndex = 0;
-// 	}
-// 	IMG_CONSTELLATION.src = arrayOfImages[currentIndex];
-// });
-
-// PREVIOUS.addEventListener("click", () => {
-// 	currentIndex--;
-// 	if (currentIndex === 0) {
-// 		currentIndex = 0;
-// 	}
-// 	IMG_CONSTELLATION.src = arrayOfImages[currentIndex];
-// });
+const divConstellationName = document.getElementById("constellationName");
 
 const getConstellations = async () => {
 	try {
@@ -33,7 +11,7 @@ const getConstellations = async () => {
 	} catch (error) {}
 };
 
-const fetchImages = async () => {
+const fetchConstellation = async () => {
 	const data = await getConstellations();
 	return data;
 };
@@ -43,13 +21,14 @@ let currentIndex = 0;
 NEXT.addEventListener("click", async () => {
 	try {
 		currentIndex++;
-		const arrayOfImages = await fetchImages();
-		console.log(arrayOfImages.length);
+		const arrayOfCOnstellations = await fetchConstellation();
+		console.log(arrayOfCOnstellations.length);
 
-		IMG_CONSTELLATION.src = arrayOfImages[currentIndex].image;
-		if (currentIndex === arrayOfImages.length) {
+		IMG_CONSTELLATION.src = arrayOfCOnstellations[currentIndex].image;
+		divConstellationName.innerText = arrayOfCOnstellations.name;
+		if (currentIndex === arrayOfCOnstellations.length) {
 			currentIndex = 0;
-			IMG_CONSTELLATION.src = arrayOfImages[currentIndex].image;
+			IMG_CONSTELLATION.src = arrayOfCOnstellations[0].image;
 		}
 	} catch (error) {
 		console.error(error);
@@ -58,14 +37,17 @@ NEXT.addEventListener("click", async () => {
 
 PREVIOUS.addEventListener("click", async () => {
 	try {
+		console.log("index begin " + currentIndex);
 		currentIndex--;
-		const arrayOfImages = await fetchImages();
-		console.log(arrayOfImages);
-		if (currentIndex === 0) {
-			currentIndex = 0;
-		}
+		const arrayOfCOnstellations = await fetchConstellation();
+		console.log(arrayOfCOnstellations[currentIndex].image);
 
-		IMG_CONSTELLATION.src = arrayOfImages[currentIndex].image;
+		console.log("index end " + currentIndex);
+		if (currentIndex < 0) {
+			currentIndex = arrayOfCOnstellations.length;
+		} else {
+			IMG_CONSTELLATION.src = arrayOfCOnstellations[currentIndex].image;
+		}
 	} catch (error) {
 		console.error(error);
 	}
