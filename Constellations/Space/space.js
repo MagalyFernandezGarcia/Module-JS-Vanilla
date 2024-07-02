b3f051de53a7d814fba6af854b29482667285a41
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-07-02 16:41:53 +0200

    adding nasa photo + create mobile version

--- Constellations/Space/space.js
+++ Constellations/Space/space.js
@@ -3,6 +3,9 @@ const PREVIOUS = document.getElementById("previous");
 const NEXT = document.getElementById("next");
 const jsonAPi = "http://localhost:3000/";
 const divConstellationName = document.getElementById("constellationName");
+const hubble = document.getElementById("hubble");
+const modalHubble = document.getElementById("modalHubble");
+const burgerMenu = document.getElementById("burgerMenu");
 
 const getConstellations = async () => {
 	try {
@@ -11,44 +14,55 @@ const getConstellations = async () => {
 	} catch (error) {}
 };
 
-const fetchConstellation = async () => {
-	const data = await getConstellations();
-	return data;
-};
-
 let currentIndex = 0;
 
-NEXT.addEventListener("click", async () => {
+PREVIOUS.addEventListener("pointerup", async () => {
 	try {
-		currentIndex++;
-		const arrayOfCOnstellations = await fetchConstellation();
-		console.log(arrayOfCOnstellations.length);
+		currentIndex--;
+		const arrayOfCOnstellations = await getConstellations();
+
+		if (currentIndex < 0) {
+			currentIndex = arrayOfCOnstellations.length - 1;
+		}
 
 		IMG_CONSTELLATION.src = arrayOfCOnstellations[currentIndex].image;
-		divConstellationName.innerText = arrayOfCOnstellations.name;
+		divConstellationName.innerText = arrayOfCOnstellations[currentIndex].name;
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+NEXT.addEventListener("pointerup", async () => {
+	try {
+		currentIndex++;
+		const arrayOfCOnstellations = await getConstellations();
+
 		if (currentIndex === arrayOfCOnstellations.length) {
 			currentIndex = 0;
-			IMG_CONSTELLATION.src = arrayOfCOnstellations[0].image;
 		}
+		IMG_CONSTELLATION.src = arrayOfCOnstellations[currentIndex].image;
+		divConstellationName.innerText = arrayOfCOnstellations[currentIndex].name;
 	} catch (error) {
 		console.error(error);
 	}
 });
+const getNasaImage = async () => {
+	const res = await axios.get(nasaAPI);
+	return res.data;
+};
 
-PREVIOUS.addEventListener("click", async () => {
-	try {
-		console.log("index begin " + currentIndex);
-		currentIndex--;
-		const arrayOfCOnstellations = await fetchConstellation();
-		console.log(arrayOfCOnstellations[currentIndex].image);
-
-		console.log("index end " + currentIndex);
-		if (currentIndex < 0) {
-			currentIndex = arrayOfCOnstellations.length;
-		} else {
-			IMG_CONSTELLATION.src = arrayOfCOnstellations[currentIndex].image;
-		}
-	} catch (error) {
-		console.error(error);
-	}
+hubble.addEventListener("pointerup", async () => {
+	const modalHubbleContent = document.querySelector(".modalHubbleContent");
+	modalHubbleContent.innerHTML = "";
+	const imageOfSpace = await getNasaImage();
+	modalHubble.style.display = "block";
+	const imageToDisplay = document.createElement("img");
+	imageToDisplay.src = imageOfSpace.hdurl;
+	modalHubbleContent.append(imageToDisplay);
 });
+
+window.addEventListener("pointerup", () => {
+	modalHubble.style.display = "none";
+});
+
+burgerMenu.addEventListener("pointerup", () => {});
