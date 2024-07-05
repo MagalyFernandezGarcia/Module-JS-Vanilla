ac05c161b9c17bedec612e883aae16c34d81fbbd
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-07-05 14:14:42 +0200

    adding tips page

---
+++ Constellations/cssTest/tips/tips.js
@@ -0,0 +1,11 @@
+const activateAccordion = () => {
+	const accordion = document.getElementsByClassName("container");
+
+	for (i = 0; i < accordion.length; i++) {
+		accordion[i].addEventListener("click", function () {
+			this.classList.toggle("active");
+		});
+	}
+};
+
+activateAccordion();
