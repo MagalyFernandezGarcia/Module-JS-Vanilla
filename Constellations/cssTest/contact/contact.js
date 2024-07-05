207c7405138bcbf993d319d22b9b681d6f6225f8
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-07-05 10:00:45 +0200

    finishing contact form

--- Constellations/cssTest/contact/contact.js
+++ Constellations/cssTest/contact/contact.js
@@ -23,48 +23,67 @@ const dataInput = (formulaire) => {
 	}
 	return allValue;
 };
-submitBtn.addEventListener("click", (event) => {
+// submitBtn.addEventListener("click", (event) => {
+// 	event.preventDefault();
+// 	const form = event.target.closest("form");
+// 	const formData = new FormData(form);
+
+// 	if (!inputName.checkValidity()) {
+// 		if (inputName.validity.tooLong) {
+// 			errorName.innerText = "Le nom ne doit pas dépasser 200 caractères.";
+// 		}
+// 		if (inputName.validity.patternMismatch) {
+// 			errorName.innerText = "Le nom ne doit pas contenir de chiffres.";
+// 		}
+// 		if (inputName.validity.valueMissing) {
+// 			errorName.innerText = "Le nom est obligatoire";
+// 		}
+
+// 		inputName.className = "errorInput";
+// 		inputName.focus();
+// 		return;
+// 	} else if (!inputFirstName.checkValidity()) {
+// 		console.log("test");
+// 		if (inputFirstName.validity.tooLong) {
+// 			errorFirstName.innerText =
+// 				"Le prénom ne doit pas dépasser 200 caractères.";
+// 		}
+// 		if (inputFirstName.validity.patternMismatch) {
+// 			errorFirstName.innerText = "Le prénom ne doit pas contenir de chiffres.";
+// 		}
+
+// 		inputFirstName.className = "errorInput";
+// 		inputFirstName.focus();
+// 		return;
+// 	} else if (inputMail.validity.valueMissing) {
+// 		errorFirstName.innerText = "Le mail est obligatoire";
+// 		return;
+// 	}
+
+// 	contactModal.style.display = "block";
+// 	const receptionDiv = document.createElement("div");
+// 	receptionDiv.innerText =
+// 		"Nous avons bien reçu votre message et vous répondrons dans les plus brefs délais. Vous trouverez-ci dessous une copie des information reçue.";
+// 	const valueOfInput = dataInput(formData);
+// 	const addingCloseBtn = document.createElement("button");
+// 	addingCloseBtn.className = "close";
+// 	addingCloseBtn.textContent = "Fermer";
+// 	addingCloseBtn.addEventListener("click", () => {
+// 		contactModal.style.display = "none";
+// 		contactModalContent.innerHTML = "";
+// 	});
+// 	contactModalContent.append(receptionDiv, valueOfInput, addingCloseBtn);
+// 	contactModal.append(contactModalContent);
+// });
+
+contactForm.addEventListener("submit", (event) => {
 	event.preventDefault();
-	const form = event.target.closest("form");
-	const formData = new FormData(form);
-
-	if (!inputName.checkValidity()) {
-		if (inputName.validity.tooLong) {
-			errorName.innerText = "Le nom ne doit pas dépasser 200 caractères.";
-		}
-		if (inputName.validity.patternMismatch) {
-			errorName.innerText = "Le nom ne doit pas contenir de chiffres.";
-		}
-		if (inputName.validity.valueMissing) {
-			errorName.innerText = "Le nom est obligatoire";
-		}
-
-		inputName.className = "errorInput";
-		inputName.focus();
-		return;
-	} else if (!inputFirstName.checkValidity()) {
-		console.log("test");
-		if (inputFirstName.validity.tooLong) {
-			errorFirstName.innerText =
-				"Le prénom ne doit pas dépasser 200 caractères.";
-		}
-		if (inputFirstName.validity.patternMismatch) {
-			errorFirstName.innerText = "Le prénom ne doit pas contenir de chiffres.";
-		}
-
-		inputFirstName.className = "errorInput";
-		inputFirstName.focus();
-		return;
-	} else if (inputMail.validity.valueMissing) {
-		errorFirstName.innerText = "Le mail est obligatoire";
-		return;
-	}
-
+	const data = new FormData(event.target);
 	contactModal.style.display = "block";
 	const receptionDiv = document.createElement("div");
 	receptionDiv.innerText =
 		"Nous avons bien reçu votre message et vous répondrons dans les plus brefs délais. Vous trouverez-ci dessous une copie des information reçue.";
-	const valueOfInput = dataInput(formData);
+	const valueOfInput = dataInput(data);
 	const addingCloseBtn = document.createElement("button");
 	addingCloseBtn.className = "close";
 	addingCloseBtn.textContent = "Fermer";
