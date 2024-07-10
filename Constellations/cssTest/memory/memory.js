4d0f58aa336ef2bb4609f1472913c7f81eb17d85
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-07-10 16:39:31 +0200

    adding logic in memory

--- Constellations/cssTest/memory/memory.js
+++ Constellations/cssTest/memory/memory.js
@@ -1,8 +1,143 @@
 const cardAll = document.querySelectorAll(".card");
+const jsonAPi = "http://localhost:3000/";
 
-cardAll.forEach((card) => {
-	const newImg = document.createElement("img");
-	newImg.src = "../../images/backCard.jpg";
-	newImg.className = "imgCard";
-	card.append(newImg);
-});
+let count = 0;
+
+const initialise = () => {
+	cardAll.forEach((card) => {
+		const newImg = document.createElement("img");
+		window.addEventListener("load", () => {
+			newImg.src = "../../images/backCard.jpg";
+		});
+		newImg.className = "imgCard";
+		newImg.id = count;
+		card.innerHTML = "";
+		card.classList.add("back");
+		card.style.display = "block";
+		card.append(newImg);
+		count++;
+	});
+	selectImg(arrayOfrandomConstellations());
+};
+
+const getConstellations = async () => {
+	try {
+		const res = await axios.get(jsonAPi + "constellations");
+		return res.data;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const arrayOfrandomConstellations = async () => {
+	const arrayOfConstellations = await getConstellations();
+
+	const finalArray = [];
+	for (let i = 0; i < 4; i++) {
+		const newConstellation =
+			arrayOfConstellations[
+				Math.floor(Math.random() * arrayOfConstellations.length)
+			];
+		const isAlreadyIn = finalArray.some(
+			(element) => element === newConstellation
+		);
+		if (isAlreadyIn === true) {
+			i--;
+		} else {
+			finalArray.push(newConstellation, newConstellation);
+		}
+	}
+
+	return finalArray;
+};
+const flipCard = (urls) => {
+	let counterClick = 0;
+	cardAll.forEach((card, index) => {
+		card.addEventListener("click", () => {
+			counterClick++;
+			const imgCard = document.getElementById(index);
+
+			if (card.classList.contains("back")) {
+				imgCard.src = urls[index];
+
+				card.classList.remove("back");
+				card.classList.add("front");
+			} else {
+				imgCard.src = "../../images/backCard.jpg";
+
+				card.classList.remove("front");
+				card.classList.add("back");
+			}
+			if (counterClick === 2) {
+				compareCards();
+				counterClick = 0;
+			}
+		});
+	});
+};
+
+const selectImg = async (array) => {
+	const waitedArray = await array;
+	const allImgCards = document.querySelectorAll(".imgCard");
+	const arrayofUrl = [];
+	allImgCards.forEach((img) => {
+		const random = Math.floor(Math.random() * waitedArray.length);
+
+		img.src = `../${waitedArray[random].imageConstellation}`;
+		arrayofUrl.push(`../${waitedArray[random].imageConstellation}`);
+		waitedArray.splice(random, 1);
+	});
+	flipCard(arrayofUrl);
+};
+
+let victoryCount = 0;
+const compareCards = () => {
+	const flippedCards = document.querySelectorAll(".front");
+
+	let urlArray = [];
+	flippedCards.forEach((flippedCard) => {
+		const imageFlipped = flippedCard.querySelector(".imgCard");
+		urlArray.push(imageFlipped.src);
+	});
+
+	if (urlArray[0] === urlArray[1]) {
+		setTimeout(() => {
+			flippedCards.forEach((flippedCard) => {
+				flippedCard.classList.remove("front");
+				flippedCard.classList.add("back");
+				flippedCard.style.display = "none";
+			});
+		}, 1000);
+		victoryCount++;
+	} else {
+		setTimeout(() => {
+			flippedCards.forEach((flippedCard) => {
+				const imageFlipped = flippedCard.querySelector(".imgCard");
+				flippedCard.classList.remove("front");
+				flippedCard.classList.add("back");
+				imageFlipped.src = "../../images/backCard.jpg";
+			});
+		}, 1000);
+	}
+
+	if (victoryCount === 1) {
+		const main = document.querySelector("main");
+		const winTextDiv = document.createElement("h2");
+		winTextDiv.innerText = "Félicitation, vous avez gagné!";
+		const resetBtn = document.createElement("button");
+		resetBtn.innerText = "Fermer";
+		resetBtn.className = "reset";
+		const winDiv = document.createElement("div");
+		winDiv.className = "winDiv";
+		winDiv.append(winTextDiv, resetBtn);
+		main.append(winDiv);
+
+		resetBtn.addEventListener("click", () => {
+			count = 0;
+
+			initialise();
+			winDiv.style.display = "none";
+		});
+	}
+};
+initialise();
